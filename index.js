76af7883da2ef3cb2d63204444c4a4a3670c42da
Humberto Rico <humbertorico@Humbertos-MacBook-Pro.local>
2022-08-15 20:18:53 -0400

    personnel

--- index.js
+++ index.js
@@ -133,14 +133,17 @@ async function run() {
                 <div class="col card-header">
                     <h4>${employeeArray[j].name}</h4>
                 </div>
+
                 <div class="col card-header">
                     <h4>${employeeArray[j].title}</h4 >
                 </div >
+
                 <ul class="list-group list-group-flush text">
                     <li class="list-group-item">ID: ${employeeArray[j].id}</li>
                     <li class="list-group-item">Email: ${employeeArray[j].email}</li>
                     <li class="list-group-item"> ${displayTitle(employeeArray[j])}</li>
                 </ul>
+
             </div > `;
         }
         return html;
@@ -150,6 +153,7 @@ async function run() {
 
     let html = `< !DOCTYPE html >
                 <html lang="en">
+
                     <head>
                         <meta charset="UTF-8">
                             <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -157,6 +161,7 @@ async function run() {
                                     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
                                         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
                                         <title>Document</title>
+
                                         <style>
                                             .row {
                                                 display: flex;
@@ -165,6 +170,7 @@ async function run() {
                 margin-top: 20px;
                 margin-bottom: 20px;
             }
+
             .card {
                                                 padding: 15px;
                 border-radius: 6px;
@@ -172,6 +178,7 @@ async function run() {
                 color: lightskyblue;
                 margin: 15px;
             }
+
             .text {
                                                 padding: 15px;
                 border-radius: 6px;
@@ -179,12 +186,14 @@ async function run() {
                 color: black;
                 margin: 15px;
             }
+
             .col {
                                                 flex: 1;
                 text-align: center;
             }
         </style>
     </head>
+
                                     <body>
                                         <nav class="navbar navbar-dark bg-dark justify-content-center align-items-center">
                                             <span class="navbar-brand mb-0 h1">
@@ -192,11 +201,16 @@ async function run() {
                                             </span>
                                         </nav>
                                         <div class="row">
+
                                             ${getCardHtml()}
+
+
                                         </div>
+
                                     </body>
     
     </html>
+
     `;
 
 
@@ -210,3 +224,5 @@ async function run() {
     });
 }
 run()
+
+
